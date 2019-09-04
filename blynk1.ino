fe5620bdb27f67aca1155d2186b4df1fc9bdaa14
Abhishek Kumar <abhishek.tom8@gmail.com>
2019-09-05 02:44:11 +0530

    Add files via upload

---
+++ blynk1.ino
@@ -0,0 +1,33 @@
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "k14KaDL4-PUNE712tY7MgylqoSk6BvY5";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "mortal"
+char pass[] = "qwerty456";
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+}
+
+void loop()
+{
+  Blynk.run();
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
+}
